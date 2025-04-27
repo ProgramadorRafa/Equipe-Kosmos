b71217b15bad1133a66ea78c205c898fddd69bad
ProgramadorRafa <rafael_contato.exe@outlook.com>
2025-04-27 01:18:23 -0300

    script menu aperfeicoado e melhor estilizado

--- js/scriptKosmos.js
+++ js/scriptKosmos.js
@@ -1,14 +1,29 @@
-const openMenuBtn = document.querySelector('.openMenu');
-let closeMenuBtn = document.querySelector('.closeMenu');
+let openMenuBtn = document.querySelector('.openMenu');
+// let closeMenuBtn = document.querySelector('.closeMenu');
+let iconMenuMobile = document.querySelector('.iconChange');
+
+
+console.log(iconMenuMobile.classList.value)
+
+
 
 function menuShow() {
     let menuMobile = document.querySelector('.menuList');
+
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
     } else{
         menuMobile.classList.add('open');
     }
 }
-
 openMenuBtn.addEventListener('click', menuShow);
-closeMenuBtn.addEventListener('click', menuShow);
+
+const adicaoFctCloseMenu = () => {
+    let menuMobile = document.querySelectorAll('.itemMenu');
+
+    menuMobile.forEach((itensMenu) => {
+        itensMenu.addEventListener('click', menuShow);
+        console.log('clicou')
+    })
+}
+openMenuBtn.addEventListener('click', adicaoFctCloseMenu);
